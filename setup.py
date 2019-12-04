f9f0bc94ade7120e701cc2db6f0c1b5374677241
Jeffrey Byrne <jebyrne@gmail.com>
2019-12-04 13:17:18 -0500

    v0.3

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 setup(
     name = 'vipy',
-    version = '0.2',
+    version = '0.3',
     packages = find_packages(),
     description = 'Visym python tools for privacy preserving computer vision',
     author = 'Jeffrey Byrne',
     author_email = 'jeff@visym.com',
     url = 'https://github.com/visym/vipy',
-    download_url = 'https://github.com/visym/vipy/archive/0.2.tar.gz', 
+    download_url = 'https://github.com/visym/vipy/archive/0.3.tar.gz', 
     install_requires=[
         "numpy",  
         "scipy",
