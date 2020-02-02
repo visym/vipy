a39f90577305076d7c06eabc33f737daf35e7a81
Byrne <jeff@visym.com>
2020-02-02 13:53:11 -0500

    refactored video unit tests

---
+++ test/test_video.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+from vipy.video import Video
+from vipy.util import tempjpg, tempdir
+from vipy.videosearch import 
+
+def run():    
+    # Common Parameters
+    mp4url = None
+
+    
+
+    # Empty constructor
+    try:
+        v = Video()
+    except:
+        print('Empty constructor: PASSED')
+
+    # URL
+    
+
+
+if __name__ == "__main__":
+    run()
+
+
+
