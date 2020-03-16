f0575fbe606d399cb692e8466ccb99b99117ed48
Byrne <jeff@visym.com>
2020-03-16 11:36:59 -0400

    version bump

--- vipy/version.py
+++ vipy/version.py
@@ -1,23 +1,22 @@
 MAJOR = 0
 MINOR = 7
-RELEASE = 4
+RELEASE = 5
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, RELEASE)
 
 
 def at_least_version(versionstring):
     """Is versionstring='X.Y.Z' at least the current version?"""
-    (major, minor, release) = versionnum.split('.')
+    (major, minor, release) = versionstring.split('.')
     return at_least_major_version(major) and at_least_minor_version(minor) and at_least_release_version(release)
 
 
 def at_least_major_version(major):
-    return int(major) >= MAJOR
+    return MAJOR >= int(major)
 
 
 def at_least_minor_version(minor):
-    return int(minor) >= MINOR
+    return MINOR >= int(minor)
 
 
 def at_least_release_version(release):
-    return int(release) >= RELEASE
-
+    return RELEASE >= int(release)
