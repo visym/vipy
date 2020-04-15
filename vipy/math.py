ba933459b15726c830dcf6cbe939f00d240de076
Byrne <jeff@visym.com>
2020-04-15 10:13:02 -0400

    find_closest_divisor

--- vipy/math.py
+++ vipy/math.py
@@ -13,13 +13,13 @@ def runningmean(X, n):
 
 
 def find_closest_divisor(a, b):
-    """Return integer divisor (bh) of (a) closest to (b) in abs(b-bh) such that a % bh == 0.  This uses exhaustive search, which is inefficient for large a."""
+    """Return non-trivial integer divisor (bh) of (a) closest to (b) in abs(b-bh) such that a % bh == 0.  This uses exhaustive search, which is inefficient for large a."""
     assert a>=b
     for k in range(0, a-b+1):
         bh = b + k
-        if a % bh == 0:
+        if bh>1 and a % bh == 0:
             return bh
         bh = b - k
-        if bh>0 and a % bh == 0:
+        if bh>1 and a % bh == 0:
             return bh
     return a  # should never get here, since bh=a is always a solution
