e8b8cc8927ae693bc22e2f43e89c1a840d08cbd3
Jeffrey Byrne <jeff@visym.com>
2020-08-23 20:07:37 -0400

    unit tests

--- test/test_image.py
+++ test/test_image.py
@@ -19,7 +19,7 @@ rgbafile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'face_rgba.p
                         
 def test_image():
     assert vipy.version.is_at_least('0.7.0')
-    assert not vipy.version.is_at_least('1.0.0')
+    assert not vipy.version.is_at_least('10.0.0')
     
     # Empty constructor should not raise exception
     im = Image()
