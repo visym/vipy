avoid clones(), fixed off by one in preview()

vipy/object.py
@@ -10,6 +10,7 @@ try:
 except ImportError:
     import json
 
+DETECTION_GUID = int(uuid.uuid4().hex[0:8], 16)  
 
 class Detection(BoundingBox):
     """vipy.object.Detection class
@@ -28,7 +29,10 @@ class Detection(BoundingBox):
     def __init__(self, label=None, xmin=None, ymin=None, width=None, height=None, xmax=None, ymax=None, confidence=None, xcentroid=None, ycentroid=None, category=None, xywh=None, shortlabel=None, attributes=None, id=True):
         super().__init__(xmin=xmin, ymin=ymin, width=width, height=height, xmax=xmax, ymax=ymax, xcentroid=xcentroid, ycentroid=ycentroid, xywh=xywh)
         assert not (label is not None and category is not None), "Constructor requires either label or category kwargs, not both"
-        self._id = uuid.uuid4().hex if id is True else (None if id is False else id)  # unique id if id=True
+        if id is True:
+            global DETECTION_GUID; self._id = hex(int(DETECTION_GUID))[2:];  DETECTION_GUID = DETECTION_GUID + 1;  # faster, increment package level UUID4 initialized GUID
+        else:
+            self._id = None if id is False else id
         self._label = category if category is not None else label
         self._shortlabel = self._label if shortlabel is None else shortlabel
         self._confidence = float(confidence) if confidence is not None else confidence
@@ -39,7 +43,8 @@ class Detection(BoundingBox):
         assert isinstance(d, BoundingBox)
         if d.__class__ != Detection:
             d.__class__ = Detection
-            d._id = uuid.uuid4().hex if flush or not hasattr(d, '_id') else d._id
+            global DETECTION_GUID; newid = hex(int(DETECTION_GUID))[2:];  DETECTION_GUID = DETECTION_GUID + 1;  
+            d._id = newid if flush or not hasattr(d, '_id') else d._id
             d._shortlabel = None if flush or not hasattr(d, '_shortlabel') else d._shortlabel
             d._confidence = None if flush or not hasattr(d, '_confidence') else d._confidence
             d._label = None if flush or not hasattr(d, '_label') else d._label
@@ -191,7 +196,7 @@ class Track(object):
     def from_json(cls, s):
         d = json.loads(s) if not isinstance(s, dict) else s
         return cls(keyframes=tuple(int(f) for f in d['_keyframes']),
-                   boxes=tuple([BoundingBox.from_json(bbs) for bbs in d['_keyboxes']]),
+                   boxes=tuple([Detection.from_json(bbs) for bbs in d['_keyboxes']]),
                    category=d['_label'],
                    framerate=d['_framerate'],
                    interpolation=d['_interpolation'],
@@ -345,10 +350,12 @@ class Track(object):
         assert len(self._keyboxes) > 0, "Degenerate object for interpolation"   # not self.isempty()
         if len(self._keyboxes) == 1:
             return Detection.cast(self._keyboxes[0].clone()).setattribute('trackid', self.id()) if (self._boundary == 'extend' or self.during(f)) else None
+        if f in reversed(self._keyframes):            
+            return self._keyboxes[self._keyframes.index(f)].setattribute('trackid', self.id())
 
         kf = self._keyframes
         ft = min(max(f, kf[0]), kf[-1])  # truncated frame index
-        for i in range(0, len(kf)-1):
+        for i in reversed(range(0, len(kf)-1)):
             if kf[i] <= ft and kf[i+1] >= ft:
                 break  # floor keyframe index
         c = (ft - kf[i]) / max(1, float(kf[i+1] - kf[i]))  # interpolation coefficient
@@ -386,7 +393,8 @@ class Track(object):
     def during(self, k_start, k_end=None):
         """Is frame during the time interval (startframe, endframe) inclusive?"""        
         k_end = k_start+1 if k_end is None else k_end
-        return len(self)>0 and any([k >= self.startframe() and k <= self.endframe() for k in range(k_start, k_end)])
+        #return len(self)>0 and any([k >= self.startframe() and k <= self.endframe() for k in range(k_start, k_end)])
+        return len(self)>0 and ((k_start >= self._keyframes[0] and k_start <= self._keyframes[-1]) or (k_end >= self._keyframes[0] and k_end <= self._keyframes[-1]))
 
     def during_interval(self, k_start, k_end):
         return self.during(k_start, k_end)
@@ -407,7 +415,7 @@ class Track(object):
         keyframes = copy.deepcopy(self.keyframes())
         for k in keyframes:
             if ((startframe is not None and k < startframe) or (endframe is not None and k >= endframe)):
-                self.delete(k)
+                self.delete(k)  # will also delete corresponding keybox
         return self
         
     def rescale(self, s):
@@ -464,10 +472,11 @@ class Track(object):
     def clone(self):
         return copy.deepcopy(self)
 
-    def boundingbox(self):
+    def boundingbox(self, startframe=None, endframe=None):
         """The bounding box of a track is the smallest spatial box that contains all of the detections within startframe and endframe, or None if there are no detections"""
-        d = self._keyboxes[0].clone() if len(self._keyboxes) >= 1 else None
-        return d.union([bb for (k,bb) in zip(self._keyframes[1:], self._keyboxes[1:]) if self.during(k)]) if (d is not None and len(self._keyboxes) >= 2) else d
+        t = self.clone() if (startframe is None and endframe is None) else self.clone().truncate(startframe, endframe)
+        d = t._keyboxes[0].clone() if len(t._keyboxes) >= 1 else None
+        return d.union([bb for (k,bb) in zip(t._keyframes[1:], t._keyboxes[1:]) if t.during(k)]) if (d is not None and len(t._keyboxes) >= 2) else d
 
     def smallestbox(self):
         """The smallest box of a track is the smallest spatial box in area along the track"""
@@ -701,10 +710,10 @@ class Track(object):
             return self.nearest_keybox(k)
         else:
             n = self.endframe() if k > self.endframe() else self.startframe()+1
+            d = self.endbox().clone() if k > self.endframe() else self.startbox().clone()
             (vx, vy) = self.shape_invariant_velocity(n, dt=dt) if not shape else self.velocity(n, dt=dt)
             (vw, vh) = (self.velocity_w(n, dt=dt), self.velocity_h(n, dt=dt)) if shape else (0,0)
-            return (self[n]
-                    .translate((k-n)*vx, (k-n)*vy)
+            return (d.translate((k-n)*vx, (k-n)*vy)
                     .top(0 if not shape else ((k-n)*vh)/2.0)
                     .bottom(0 if not shape else ((k-n)*vh)/2.0)
                     .left(0 if not shape else ((k-n)*vw)/2.0)
@@ -743,7 +752,8 @@ class Track(object):
 
     def bearing_change(self, f1=None, f2=None, dt=30, eps=1E-9):
         """The bearing change of a track from frame f1 (or start) and frame f2 (or end) is the relative angle of the velocity vectors in radians [-pi,pi]"""
-        dr = self.bearing(f1 if f1 is not None else self.startframe()+dt, dt) - self.bearing(f2 if f2 is not None else self.endframe(), dt)  
+        dt = min(dt, len(self))
+        dr = self.bearing(f1 if f1 is not None else min(self.endframe(), self.startframe()+dt), dt) - self.bearing(f2 if f2 is not None else self.endframe(), dt)  
         return dr if np.abs(dr)<=np.pi else ((2*np.pi - dr) if (dr > np.pi) else (2*np.pi + dr))
         
     def velocity(self, f, dt=30):
@@ -758,7 +768,8 @@ class Track(object):
     def shape_invariant_velocity(self, f, dt=30):
         """Return the (x,y) track velocity at frame f in units of pixels per frame computed by minimum mean finite differences of any box corner independent of changes in shape"""
         assert f >= 0 and dt > 0 and self.during(f) 
-        bbl = [self[f]] + [self[f-k] for k in range(1,dt) if self.during(f-k)]
+        #bbl = [self[f]] + [self[f-k] for k in range(1,dt) if self.during(f-k)]
+        bbl = [self.nearest_keybox(k) for k in range(f,f-dt,-1) if self.during(k)]
         vx = float(np.mean([sorted([(bbl[0].ulx() - bbl[k].ulx())/float(k), 
                                     (bbl[0].urx() - bbl[k].urx())/float(k), 
                                     (bbl[0].blx() - bbl[k].blx())/float(k), 
@@ -816,14 +827,14 @@ def non_maximum_suppression(detlist, conf, iou, bycategory=False, cover=None, co
     suppressed = set([])
     detlist = [d for d in detlist if d.confidence() > conf and not d.isdegenerate()]  # valid
     detlist = sorted(detlist, key=lambda d: d.confidence(), reverse=True)  # biggest to smallest
-    detlist = [(d.clone().dilate(coverdilation), d) for d in detlist]  
-    for i in range(0, len(detlist)-1):
-        for j in range(i+1, len(detlist)):
-            if i not in suppressed and j not in suppressed:
-                ((ddi, di), (ddj, dj)) = (detlist[i], detlist[j])
-                if (bycategory is False or di.category() == dj.category()) and (di.iou(dj) >= iou or (cover is not None and ddj.cover(di) >= cover)):
-                    suppressed.add(j)
-    return sorted([d for (j,(dd,d)) in enumerate(detlist) if j not in suppressed], key=lambda x: x.confidence())  # smallest to biggest confidence for display layering
+    for (i, di) in enumerate(detlist):
+        if i in suppressed:
+            continue
+        ddi = di.clone().dilate(coverdilation)
+        for (j, dj) in enumerate(detlist[i+1:], start=i+1):
+            if (j not in suppressed) and (bycategory is False or di.category() == dj.category()) and ddi.hasintersection(dj) and ((cover is not None and ddi.cover(dj) >= cover) or di.iou(dj) >= iou):
+                suppressed.add(j)
+    return sorted([d for (j,d) in enumerate(detlist) if j not in suppressed], key=lambda x: x.confidence())  # smallest to biggest confidence for display layering
 
 
 def greedy_assignment(srclist, dstlist, miniou=0.0, bycategory=False):

vipy/video.py
@@ -249,7 +249,7 @@ class Video(object):
             def __init__(self, v):
                 self._video = v   # do not clone
                 self._pipe = None
-                self._frame_index = None
+                self._frame_index = 0
                 self._vcodec = 'libx264'
                 self._framerate = self._video.framerate()
                 self._outfile = self._video.filename()
@@ -283,7 +283,8 @@ class Video(object):
                                 queue.put(None)
                                 return None
                             else:
-                                queue.put(np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3]))
+                                img = np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3])
+                                queue.put(img)
 
                     self._queue = queue.Queue(128)
                     (height, width) = self._shape
@@ -878,9 +879,10 @@ class Video(object):
         #   - This means that frame indexing from preview() will generate slightly different images than streaming raw
         #   - Beware running convnets, as the pixels will be slightly different (~4 grey levels in uint8) ... 
         try:
-            # FFMPEG frame indexing is inefficient for large framenum.  Need to add "-ss sec.msec" flag before input, but this can screw up clip timing, so this cannot be used in general
+            # FFMPEG frame indexing is inefficient for large framenum.  Need to add "-ss sec.msec" flag before input
             #   - the "ss" option must be provided before the input filename, and is supported by ffmpeg-python as ".input(in_filename, ss=time)"
-            timestamp_in_seconds = framenum/float(self.framerate())
+            #   - Seek to the frame before the desired frame in order to pipe the next (desired) frame 
+            timestamp_in_seconds = max(0.0, (framenum-1)/float(self.framerate()))
             f_prepipe = self.clone(shallow=True)._update_ffmpeg_seek(offset=timestamp_in_seconds)._ffmpeg.filter('select', 'gte(n,{})'.format(0))
             f = f_prepipe.output('pipe:', vframes=1, format='image2', vcodec='mjpeg')\
                          .global_args('-cpuflags', '0', '-loglevel', 'debug' if vipy.globals.isdebug() else 'error')
@@ -1705,10 +1707,10 @@ class Scene(VideoCategory):
     def frame(self, k, img=None):
         """Return vipy.image.Scene object at frame k"""
         assert isinstance(k, int) and k>=0, "Frame index must be non-negative integer"
-        assert img is not None or (self.isloaded() and k<len(self)) or not self.isloaded(), "Invalid frame index %d - Indexing video by frame must be integer within (0, %d)" % (k, len(self))
+        assert img is not None or (self.isloaded() and k<len(self)) or not self.isloaded(), "Invalid frame index %d - Indexing video by frame must be integer within (0, %d)" % (k, len(self)-1)
 
         img = img if img is not None else (self._array[k] if self.isloaded() else self.preview(k).array())
-        dets = [t[k].setattribute('trackindex', j) for (j,(tid,t)) in enumerate(self.tracks().items()) if t[k] is not None]  # track interpolation (cloned) with boundary handling
+        dets = [t[k].clone().setattribute('trackindex', j) for (j, t) in enumerate(self.tracklist()) if len(t)>0 and (t.during(k) or t.boundary()=='extend')]  # track interpolation (cloned) with boundary handling 
         for d in dets:
             shortlabel = [(d.shortlabel(),'')]  # [(Noun, Verbing1), (Noun, Verbing2), ...]
             activityconf = [None]   # for display 
@@ -1877,9 +1879,9 @@ class Scene(VideoCategory):
 
     def rekey(self):
         """Change the track and activity IDs to randomly assigned UUIDs.  Useful for cloning unique scenes"""
-        d_old_to_new = {k:uuid.uuid4().hex for (k,a) in self.activities().items()}
+        d_old_to_new = {k:hex(int(uuid.uuid4().hex[0:8], 16))[2:] for (k,a) in self.activities().items()}
         self._activities = {d_old_to_new[k]:a.id(d_old_to_new[k]) for (k,a) in self.activities().items()}
-        d_old_to_new = {k:uuid.uuid4().hex for (k,t) in self.tracks().items()}
+        d_old_to_new = {k:hex(int(uuid.uuid4().hex[0:8], 16))[2:] for (k,t) in self.tracks().items()}
         self._tracks = {d_old_to_new[k]:t.id(d_old_to_new[k]) for (k,t) in self.tracks().items()}
         for (k,v) in d_old_to_new.items():
             self.activitymap(lambda a: a.replaceid(k,v) )
@@ -1967,19 +1969,18 @@ class Scene(VideoCategory):
             t = vipy.object.Track(category=obj.category(), keyframes=[self._currentframe], boxes=[obj], boundary='strict', attributes=obj.attributes)
             if rangecheck and not obj.hasoverlap(width=self.width(), height=self.height()):
                 raise ValueError("Track '%s' does not intersect with frame shape (%d, %d)" % (str(t), self.height(), self.width()))
-            self.tracks()[t.id()] = t  
+            self.tracks()[t.id()] = t  # by-reference
             return t.id()
         elif isinstance(obj, vipy.object.Track):
             if rangecheck and not obj.boundingbox().isinside(vipy.geometry.imagebox(self.shape())):
                 obj = obj.imclip(self.width(), self.height())  # try to clip it, will throw exception if all are bad 
                 warnings.warn('Clipping track "%s" to image rectangle' % (str(obj)))
-            self.tracks()[obj.id()] = obj
+            self.tracks()[obj.id()] = obj  # by-reference
             return obj.id()
         elif isinstance(obj, vipy.activity.Activity):
             if rangecheck and obj.startframe() >= obj.endframe():
                 raise ValueError("Activity '%s' has invalid (startframe, endframe)=(%d, %d)" % (str(obj), obj.startframe(), obj.endframe()))
-            self.activities()[obj.id()] = obj
-            # FIXME: check to see if activity has at least one track during activity
+            self.activities()[obj.id()] = obj  # by-reference, activity may have no tracks
             return obj.id()
         elif (istuple(obj) or islist(obj)) and len(obj) == 4 and isnumber(obj[0]):
             assert self._currentframe is not None, "add() for obj=xywh must be added during frame iteration (e.g. for im in video: )"
@@ -1987,7 +1988,7 @@ class Scene(VideoCategory):
             if rangecheck and not t.boundingbox().isinside(vipy.geometry.imagebox(self.shape())):
                 t = t.imclip(self.width(), self.height())  # try to clip it, will throw exception if all are bad 
                 warnings.warn('Clipping track "%s" to image rectangle' % (str(t)))
-            self.tracks()[t.id()] = t
+            self.tracks()[t.id()] = t  # by-reference
             return t.id()
         else:
             raise ValueError('Undefined object type "%s" to be added to scene - Supported types are obj in ["vipy.object.Detection", "vipy.object.Track", "vipy.activity.Activity", "[xmin, ymin, width, height]"]' % str(type(obj)))
@@ -2640,7 +2641,7 @@ class Scene(VideoCategory):
                         break
         return self
 
-    def assign(self, frame, dets, minconf=0.2, maxhistory=5, activityiou=0.5, trackcover=0.2, trackconfsamples=100):
+    def assign(self, frame, dets, minconf=0.2, maxhistory=5, activityiou=0.5, trackcover=0.2, trackconfsamples=8):
         """Assign a list of vipy.object.Detections at frame k to scene by greedy track association. In-place update.
         
            * miniou [float]: the minimum temporal IOU for activity assignment
@@ -2669,27 +2670,28 @@ class Scene(VideoCategory):
         #   - Each track is assigned at most one detection
         #   - Each detection is assigned to at most one track.  
         #   - Assignment is the highest confidence maximum overlapping detection within tracking gate
+        trackconf = {t.id():t.confidence(samples=trackconfsamples) for (t, ti) in t_ref}
         assignments = [(t, d.confidence(), d.iou(ti), d.shapeiou(ti), max(d.cover(ti), ti.cover(d)), d)
                        for (t, ti) in t_ref
                        for d in objdets
-                       if t.category() == d.category()]
+                       if t.category() == d.category() and ti.hasintersection(d)]
         assigned = set([])        
         posconf = min([d.confidence() for d in objdets]) if len(objdets)>0 else 0
-        for (t, conf, iou, shapeiou, cover, d) in sorted(assignments, key=lambda x: (x[1]+posconf)*(x[2]+x[3]+x[4])+x[0].confidence(samples=trackconfsamples), reverse=True):
+        for (t, conf, iou, shapeiou, cover, d) in sorted(assignments, key=lambda x: (x[1]+posconf)*(x[2]+x[3]+x[4])+trackconf[x[0].id()], reverse=True):
             if cover > (trackcover if len(t)>1 else 0):  # the highest confidence detection within the iou gate (or any overlap if not yet enough history for velocity estimate) 
                 if (t.id() not in assigned and d.id() not in assigned):  # not assigned yet, assign it!
-                    self.track(t.id()).add(frame, d.clone())  # track assignment!
+                    self.track(t.id()).add(frame, d.clone())  # track assignment! (clone required)
                     assigned.add(t.id())  # cannot assign again to this track
                     assigned.add(d.id())  # mark detection as assigned
                 
         # Track spawn from unassigned detections
         for d in objdets:
             if d.confidence() >= minconf and d.id() not in assigned:
-                self.add(vipy.object.Track(keyframes=[frame], boxes=[d.clone()], category=d.category(), framerate=self.framerate()), rangecheck=False)
+                self.add(vipy.object.Track(keyframes=[frame], boxes=[d.clone()], category=d.category(), framerate=self.framerate()), rangecheck=False)  # clone required
                     
         # Non-maximum suppression
         deleted = set([])
-        trackconf = [(t.confidence(samples=trackconfsamples), t) for t in self.tracklist() if ((frame - t.endframe()) <= maxhistory)]
+        trackconf = [(trackconf[t.id()] if t.id() in trackconf else t.confidence(samples=trackconfsamples), t) for t in self.tracklist() if ((frame - t.endframe()) <= maxhistory)]
         for (ci, ti) in sorted(trackconf, key=lambda x: x[0], reverse=True):
             for (cj, tj) in trackconf:
                 if (ti.category() == tj.category()) and (ti.id() != tj.id()) and (tj.id() not in deleted and ti.id() not in deleted) and (cj < ci):
@@ -2702,16 +2704,16 @@ class Scene(VideoCategory):
         # Activity assignment
         assert len(activitydets) == 0 or all([d.actorid() in self.tracks() for d in activitydets]), "Invalid activity"
         assigned = []
-        for d in activitydets:
-            for a in self.activities().values():
+        for d in sorted(activitydets, key=lambda a: a.startframe()):
+            for a in sorted(self.activitylist(), key=lambda a: a.startframe()):
                 if (a.category() == d.category()) and (a.actorid() == d.actorid()) and a.hasoverlap(d, activityiou): 
                     a.union(d)  # activity assignment
                     assigned.append(d.id())
-
+        
         # Activity construction from unassigned detections
         for d in activitydets:
             if d.id() not in assigned:
-                self.add(d)  
+                self.add(d.clone())  
 
         return self
 