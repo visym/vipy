512c8586a168a2cf06a4e6f36f7e948ff979d83f
Byrne <jeff@visym.com>
2021-01-22 20:20:44 -0500

    optional numba

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     ],
     extras_require={
         'all': ['scikit-build', 'scipy', 'opencv-python', 'torch', 'ipython', 'scikit-learn', 'boto3', 'youtube-dl', 'dask', 'distributed', 'h5py', 'nltk', 'bs4', 'dropbox', 'pyyaml', 'pytest', 'paramiko', 'scp', 'ujson'],
-        'complete': ['scikit-build', 'scipy', 'opencv-python', 'torch', 'ipython', 'scikit-learn', 'boto3', 'youtube-dl', 'dask', 'distributed', 'h5py', 'nltk', 'bs4', 'dropbox', 'pyyaml', 'pytest', 'paramiko', 'scp', 'ujson']
+        'complete': ['scikit-build', 'scipy', 'opencv-python', 'torch', 'ipython', 'scikit-learn', 'boto3', 'youtube-dl', 'dask', 'distributed', 'h5py', 'nltk', 'bs4', 'dropbox', 'pyyaml', 'pytest', 'paramiko', 'scp', 'ujson', 'numba']
         },
     keywords=['computer vision machine learning ML CV privacy video image'],    
     classifiers=[
