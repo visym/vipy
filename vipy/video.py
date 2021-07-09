2fa6262d785bcce50bc6b7823912990f517bdb51
Jeffrey Byrne <jeff@visym.com>
2021-07-09 13:50:58 -0400

    youtube live stream

--- vipy/video.py
+++ vipy/video.py
@@ -347,6 +347,14 @@ class Video(object):
         >>>     for im in vi.stream():
         >>>         s.write(im)  # manipulate pixels of im, if desired
 
+        Create a YouTube live stream from an RTSP camera at 5Hz 
+        
+        >>> vo = vipy.video.Scene(url='rtmp://a.rtmp.youtube.com/live2/$SECRET_STREAM_KEY')
+        >>> vi = vipy.video.Scene(url='rtsp://URL').framerate(5)
+        >>> with vo.framerate(5).stream(write=True) as s:
+        >>>     for im in vi.framerate(5).stream():
+        >>>         s.write(im.mindim(256))
+
         Args:
             write: [bool]  If true, create a write stream
             overwrite: [bool]  If true, and the video output filename already exists, overwrite it
@@ -382,12 +390,14 @@ class Video(object):
                 if self._shape is not None:
                     (height, width) = self._shape
                     outfile = self._outfile if self._outfile is not None else self._url  # may be youtube/twitch live stream
-                    self._write_pipe = (ffmpeg.input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height), r=self._framerate) 
-                                              .filter('pad', 'ceil(iw/2)*2', 'ceil(ih/2)*2') 
-                                              .output(filename=self._outfile if self._outfile is not None else self._url, pix_fmt='yuv420p', vcodec=self._vcodec, f='flv' if vipy.util.isRTMPurl(outfile) else vipy.util.fileext(outfile, withdot=False)) 
-                                              .overwrite_output() 
-                                              .global_args('-cpuflags', '0', '-loglevel', 'quiet' if not vipy.globals.isdebug() else 'debug') 
-                                              .run_async(pipe_stdin=True))
+                    fiv = (ffmpeg.input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height), r=self._video.framerate()) 
+                           .filter('pad', 'ceil(iw/2)*2', 'ceil(ih/2)*2'))
+                    fi = ffmpeg.concat(fiv.filter('fps', fps=30, round='up'), ffmpeg.input('anullsrc', f='lavfi'), v=1, a=1) if isRTMPurl(outfile) else fiv  # empty audio for youtube-live
+                    fo = (fi.output(filename=self._outfile if self._outfile is not None else self._url, pix_fmt='yuv420p', vcodec=self._vcodec, f='flv' if vipy.util.isRTMPurl(outfile) else vipy.util.fileext(outfile, withdot=False))
+                          .overwrite_output() 
+                          .global_args('-cpuflags', '0', '-loglevel', 'quiet' if not vipy.globals.isdebug() else 'debug'))
+                    self._write_pipe = fo.run_async(pipe_stdin=True)
+                    
                 self._frame_index = 0
                 return self
             
