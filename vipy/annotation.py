a977bae92ccb49a969e9764eab623d7add63d677
Jeffrey Byrne <jeff@visym.com>
2022-02-12 15:27:39 -0500

    vipy.annotation.Selector() HTML based image group selection

--- vipy/annotation.py
+++ vipy/annotation.py
@@ -3,13 +3,16 @@ import os
 import re
 import random
 import math
-from vipy.util import try_import, isurl, remkdir
+from vipy.util import try_import, isurl, remkdir, temphtml, tempjson, filetail, readjson, isjsonfile
 import tempfile
 import numpy as np
 from vipy.downloader import common_user_agents
 from vipy.globals import print
 import warnings
-
+from datetime import datetime
+import time
+import webbrowser
+            
 
 def googlesearch(tag):
     """Return a list of image URLs from google image search associated with the provided tag"""
@@ -64,3 +67,105 @@ def verbs():
     return verbnet.lemmas()
 
 
+class Selector():
+    """HTML+javascript based UI for selecting images from a group.  
+
+    * Given a list of lists of `vipy.image.Image` objects, display each image in a standalone HTML file.  
+    * Allow the user to click on desirable images to select them.  
+    * When done, the user selects the Download JSON button to export the selected elements.
+    * The selected elements are stored in a JSON file which is exported back into this object to return only the selected images
+
+    ```python
+    imlist = [[vipy.image.RandomScene(128,128) for k in range(10)] for j in range(5)]
+    s = vipy.annotation.Selector(imlist)
+    s.html(show=True)  # annotate locally in browser by selecting images to keep, then download the resulting jsonfile
+    selected_imlist = s.fromjson('/path/to/downloaded.json')  # returns only those images selected in the UI
+    ```
+    """
+       
+    def __init__(self, imlist):
+        self._imlist = imlist
+        
+    def html(self, jsonfile=None, htmlfile=None, title='Image Selector', num_images_per_row=10, default_opacity=0.5, selected_opacity=1.0, show=False, description="Select the images to keep from each group, download the JSON, then call vipy.annotation.Select.fromjson()", mindim=None):
+        """Given a list of tuples of `vipy.image.Image` objects, create a standalone HTML file that will allow the user to select individual images in the group.  Selected images are output to jsonfile"""
+        htmlfile = temphtml() if htmlfile is None else htmlfile
+        jsonfile = filetail(tempjson()) if jsonfile is None else jsonfile
+        imlist = self._imlist
+        
+        f = open(htmlfile,'w')
+        f.write('<!DOCTYPE html>\n')
+        f.write('<!--\n    Visym Labs\n    vipy.annotation.Selector (https://visym.github.io/vipy)\n    Generated: %s\n-->\n' % str(datetime.now()))    
+        f.write('<html>\n')
+        f.write('<body>\n')
+        f.write('  <script type="text/javascript">\n')
+        f.write('    var selected = {};\n');
+        f.write('    function toggle(x) { if (x.getAttribute("selected") == "off") { x.style.opacity=%s; x.setAttribute("selected","on"); selected[x.id]=true; } else { x.style.opacity=%s; x.setAttribute("selected","off"); selected[x.id]=false;}; }\n' % (selected_opacity, default_opacity))
+        f.write('    function exportJSON(x) { var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(selected)); x.setAttribute("href", "data:"+data); x.setAttribute("download", "%s"); }\n' % jsonfile)
+        f.write('  </script>\n')    
+        f.write('<div id="container" style="width:2400px">\n')
+        f.write('<div id="header">\n')
+        f.write('<h1 style="margin-bottom:0;">%s</h1><br>\n' % title)
+        localtime = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
+        f.write('HTML generated on %s<br>\n\n' % localtime)
+        f.write('Number of image groups: %d<br>\n' % len(imlist))
+        f.write('Number of images: %d<br><br>\n' % len([im for iml in imlist for im in iml]))
+        f.write('%s<br><br>\n' % description)
+        f.write('<a onclick="exportJSON(this);" class="btn"><button onclick="exportJson(this);">Download JSON</button></a>\n') 
+        f.write('</div>\n')
+        f.write('<br>\n')
+        f.write('<hr>\n')
+        f.write('<div style="float:left;">\n')
+    
+        # Generate embedded images and html
+        for (i, imsrclist) in enumerate(imlist):
+            f.write('<p>\n</p>\n')
+            for (j, im) in enumerate(imsrclist):
+                im = im if mindim is None else im.clone().mindim(mindim)
+                f.write(im.html(id='(%d,%d)' % (i,j), attributes={'loading':'lazy', 'onclick':"toggle(this)", 'selected':'off', 'style':"opacity:%f" % default_opacity}))   # base-64 encoded image with img tag
+                if j>0 and ((j % num_images_per_row) == 0):
+                    f.write('<p>\n</p>\n')                
+            f.write('<p>\n</p>\n')
+            f.write('<br>\n')
+            f.write('<hr>\n')
+        f.write('</div>\n')
+        f.write('</body>\n')
+        f.write('</html>\n')
+        f.close()
+
+        if show:
+            webbrowser.open('file:///%s' % htmlfile)  # chrome only, will not work in safari
+        return htmlfile
+    
+    def fromjson(self, jsonfile):
+        """Given the JSON file downloaded from the HTML selector, return only those that were selected"""
+        assert isjsonfile(jsonfile)
+        d = readjson(jsonfile)
+        return [[im for (j,im) in enumerate(iml) if '(%d,%d)'%(i,j) in d and d['(%d,%d)'%(i,j)] == True] for (i,iml) in enumerate(self._imlist)]
+
+    
+class Filter(Selector):
+    """HTML+javascript based UI for filtering images from a group.  
+
+    * Given a list of lists of `vipy.image.Image` objects, display each image in a standalone HTML file.  
+    * Allow the user to click on non-desirable images to remove them.  
+    * When done, the user selects the Download JSON button to export the filtered elements.
+    * The filtered elements are stored in a JSON file which is exported back into this object to return all but the filtered images
+
+    ```python
+    imlist = [[vipy.image.RandomScene(128,128) for k in range(10)] for j in range(5)]
+    s = vipy.annotation.Filter(imlist)
+    s.html(show=True)  # annotate locally in browser by selecting images to remove and download the resulting jsonfile
+    filtered_imlist = s.fromjson('/path/to/downloaded.json')   # returns images with those selected in the UI removed
+    ```
+    """
+    
+    def html(self, jsonfile=None, htmlfile=None, title='Image Filter', num_images_per_row=10, default_opacity=1.0, selected_opacity=0.4, show=False, description="Select the images to remove from each group, download the JSON, then call vipy.annotation.Filter.fromjson()", mindim=None):
+        return super().html(jsonfile=jsonfile, htmlfile=htmlfile, title=title, num_images_per_row=num_images_per_row, default_opacity=default_opacity, selected_opacity=selected_opacity, show=show, description=description, mindim=mindim)
+
+    def fromjson(self, jsonfile):    
+        """Given the JSON file downloader from the HTML selector, return a filter of only those that were *not* selected"""
+        assert isjsonfile(jsonfile)
+        d = readjson(jsonfile)
+        return [[im for (j,im) in enumerate(iml) if '(%d,%d)'%(i,j) not in d or d['(%d,%d)'%(i,j)] == False] for (i,iml) in enumerate(self._imlist)]
+            
+    
