9685e4b958003df9f1f9427d8d0303306bafb5e4
Jeffrey Byrne <jeff@visym.com>
2024-05-17 23:14:18 -0400

    vipy.data.openimages.open_images_v7

--- vipy/data/openimages.py
+++ vipy/data/openimages.py
@@ -8,14 +8,13 @@ import io
 
 def open_images_v7(datadir=None):
     """https://storage.googleapis.com/openimages/web/download_v7.html
-
      https://github.com/cvdfoundation/open-images-dataset#download-full-dataset-with-google-storage-transfer
      https://github.com/Tencent/tencent-ml-images?tab=readme-ov-file#download-images
 
     ```python
-     (trainset, valset, testset) = open_images_v7(labeldir='/path/to/downloaded.csv')
-     d = trainset.take(128)
-     vipy.visualize.montage(d.map(lambda im: im.load(ignoreErrors=True)).filter(lambda im: im.isloaded()).map(lambda im: im.centersquare()).list()).show()
+     (trainset, valset) = open_images_v7()
+     d = trainset.take(128).map(lambda im: im.load(ignoreErrors=True)).filter(lambda im: im.isloaded())
+     vipy.visualize.montage(d.map(lambda im: im.centersquare()).list()).show()
     ```
 
     """
@@ -26,15 +25,15 @@ def open_images_v7(datadir=None):
     d_label_to_category = {r[0]:r[1] for r in vipy.util.readcsv(os.path.join(labeldir, 'oidv7-class-descriptions.csv'), ignoreheader=True)}
     d_category_to_label = {v:k for (k,v) in d_label_to_category.items()}
 
-    d_imageid_to_label = {r[0]:r[2] for r in vipy.util.readcsv(os.path.join(labeldir, 'oidv7-train-annotations-human-imagelabels.csv'), ignoreheader=True)}
-    d_val_imageid_to_label = {r[0]:r[2] for r in vipy.util.readcsv(os.path.join(labeldir, 'oidv7-val-annotations-human-imagelabels.csv'), ignoreheader=True)}    
-    d_imageid_to_label.update(d_val_imageid_to_label)
+    d_train_imageid_to_labels = vipy.util.groupbyasdict([(r[0], r[2]) for r in vipy.util.readcsv(os.path.join(labeldir, 'oidv7-train-annotations-human-imagelabels.csv'), ignoreheader=True) if float(r[3])>0], keyfunc=lambda x: x[0], valuefunc=lambda x: x[1])
+    d_val_imageid_to_labels = vipy.util.groupbyasdict([(r[0], r[2]) for r in vipy.util.readcsv(os.path.join(labeldir, 'oidv7-val-annotations-human-imagelabels.csv'), ignoreheader=True) if float(r[3])>0], keyfunc=lambda x: x[0], valuefunc=lambda x: x[1])        
+    d_imageid_to_labels = d_train_imageid_to_labels | d_val_imageid_to_labels
     
-    d_train_url_to_category = {r[2]:d_label_to_category[d_imageid_to_label[r[0]]] for r in vipy.util.readcsv(os.path.join(labeldir, 'oidv6-train-images-with-labels-with-rotation.csv'), ignoreheader=True)}
-    d_val_url_to_category = {r[2]:d_label_to_category[d_imageid_to_label[r[0]]] for r in vipy.util.readcsv(os.path.join(labeldir, 'validation-images-with-rotation.csv'), ignoreheader=True)}    
+    d_train_url_to_category = {r[2]:[d_label_to_category[c] for c in d_imageid_to_labels[r[0]]] for r in vipy.util.readcsv(os.path.join(labeldir, 'oidv6-train-images-with-labels-with-rotation.csv'), ignoreheader=True) if r[0] in d_train_imageid_to_labels}
+    d_val_url_to_category = {r[2]:[d_label_to_category[c] for c in d_imageid_to_labels[r[0]]] for r in vipy.util.readcsv(os.path.join(labeldir, 'validation-images-with-rotation.csv'), ignoreheader=True) if r[0] in d_val_imageid_to_labels}
 
-    trainset = [(url,category) for (url, category) in d_train_url_to_category.items() if d_category_to_label[category] in trainable]
-    valset = [(url,category) for (url, category) in d_val_url_to_category.items() if d_category_to_label[category] in trainable]
+    trainset = [(url,[c for c in category if d_category_to_label[c] in trainable]) for (url, category) in d_train_url_to_category.items()]
+    valset = [(url,[c for c in category if d_category_to_label[c] in trainable]) for (url, category) in d_val_url_to_category.items()]
     
     loader = lambda r: vipy.image.ImageCategory(url=r[0], category=r[1])    
     return (vipy.dataset.Dataset(trainset, id='open_images_v7:train', loader=loader),
