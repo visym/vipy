5017a3233bb80e96965e7ddb1d4a84baa1abdc20
Jeffrey Byrne <jeff@visym.com>
2024-05-26 19:31:58 -0400

    vipy.data.hf.coyo300m

--- vipy/data/hf.py
+++ vipy/data/hf.py
@@ -164,3 +164,22 @@ def imageinwords():
 def docci():
     """https://huggingface.co/datasets/google/docci"""
     dataset = load_dataset("google/docci")
+
+
+def coyo300m(threshold=0.2):
+    dataset = load_dataset("kakaobrain/coyo-labeled-300m")
+
+    labels = vipy.util.readjson(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'coyo300m.json'))    
+    d_idx_to_category = {k:[c.strip() for c in v.split(',')] for (k,v) in labels['class_description'].items()}
+    d_idx_to_wnid = {v:k for (k,v) in labels['class_list'].items()}
+    loader = lambda r, d_idx_to_wnid=d_idx_to_wnid, d_idx_to_category=d_idx_to_category, threshold=threshold: vipy.image.ImageCategory(url=r['url'], 
+                                                                                                                                       category=[d_idx_to_category[str(c)] for (c,p) in zip(r['labels'], r['label_probs']) if float(p)>threshold],
+                                                                                                                                       attributes={'wordnet':[d_idx_to_wnid[c] for (c,p) in zip(r['labels'], r['label_probs']) if float(p)>threshold]})
+    return vipy.dataset.Dataset(dataset['train'], id='coyo300m', loader=loader, strict=False)
+    
+
+def coyo700m():
+    dataset = load_dataset("kakaobrain/coyo-700m")
+
+def as100m():
+    dataset = load_dataset("OpenGVLab/AS-100M")
